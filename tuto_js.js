e236e1ca76df9908e4a13b9435334e0fd3d32c0a
delorme <delormefabien33@gmail.com>
2022-04-14 18:17:27 +0200

    bases js

--- tuto_js.js
+++ tuto_js.js
@@ -1,2 +1,22 @@
-var truc = 2;
-console.log(truc);
+var a =10, b = 2;
+
+a += b;
+console.log(a);
+
+var chips = prompt("combien avez vous de chips");
+var tomate = prompt("combien avez vous de tomate");
+
+var result = parseInt(chips) + parseInt(tomate);
+
+console.log("vous avez au total : " + result + " chips et tomates");
+
+
+
+var a =10, b = 2;
+var result = a > b ? "A > B" : "B < A";
+console.log(result);
+
+
+
+var tab = ["truc", "chose", "bidule", "george"];
+console.log(tab.slice(1,3));
